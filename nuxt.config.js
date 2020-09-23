da6c05be617509b7ddf49511a299a682dfb76455
old <meldun.kitty@gmail.com>
2020-09-23 16:29:12 +0800

    [build]

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,8 @@
 export default {
     target: 'static',
+    router: {
+        base: '/itswriggle.github.io/'
+    },
     // Global page headers (https://go.nuxtjs.dev/config-head)
     head: {
         title: 'its_wriggle',
