bf03553023c0e99db954ad130bfef88cce1871e6
old <meldun.kitty@gmail.com>
2020-09-24 11:47:31 +0800

    [build] 1.0.0

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,8 @@ export default {
 
     // Global CSS (https://go.nuxtjs.dev/config-css)
     css: [
-        'vuesax/dist/vuesax.css'
+        'vuesax/dist/vuesax.css',
+        '@/assets/scss/app.scss',
     ],
 
     // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
